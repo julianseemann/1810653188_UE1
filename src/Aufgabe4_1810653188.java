bac0d29aa9670c7e57f2d409e78a3e2f817de1bf
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2018-11-15 16:40:46 +0100

    third commit

---
+++ src/Aufgabe4_1810653188.java
@@ -0,0 +1,19 @@
+public class Aufgabe4_1810653188
+{
+    public static void main (String[] args)
+    {
+        Integer zahl1= 5;
+        Integer zahl2= 8;
+
+        System.out.println(zahl1 < zahl2);
+        System.out.println(zahl2 < zahl1);
+        System.out.println(zahl1 > zahl2);
+        System.out.println(zahl2 > zahl1);
+        System.out.println(zahl1 <= zahl2);
+        System.out.println(zahl2 <= zahl1);
+        System.out.println(zahl1 >= zahl2);
+        System.out.println(zahl2 >= zahl1);
+        System.out.println(zahl1 == zahl2);
+        System.out.println(zahl1 != zahl2);
+    }
+}
