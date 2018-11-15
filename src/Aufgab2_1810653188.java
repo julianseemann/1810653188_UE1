62bff6be5c6669d3dd5cd0e6d41ff938729bed37
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2018-11-15 15:34:14 +0100

    first commit

---
+++ src/Aufgab2_1810653188.java
@@ -0,0 +1,19 @@
+
+public class Aufgab2_1810653188;
+{
+    public static void main (String[] args)
+        {
+        String name="Julian"; //Text
+        int zahl=10; //-2.147.483.648 bis 2.147.483.647
+        double dezimalzahl=2.5; //4.9*10^-324 bis +1.7*10^308
+        boolean esIst12Uhr=false; //false, true
+        long langezahl=33452; //−9,223,372,036,854,775,808 bis +9,223,372,036,854,775,807
+        char zeichen='%'; //65.536 unterschiedliche Zeichen
+        byte zahl=20; //-128 bis 127
+        short negativeZahl=-303242; //-32.768 bis 32.767
+        float zahl=23; //-3.4*10^38 bis +3,4*10^38
+
+
+        Integer wert=218; //Konstanten haben einen fixen Wert Variablen hingegen können verändert werden.
+        }
+}
